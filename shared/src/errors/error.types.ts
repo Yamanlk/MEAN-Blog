ca1fa76d771e93e2955622be2f5f206c1ee5099a
yaman <yamanlakis@gmail.com>
2019-10-05 19:00:41 +0300

    testing new approach of error handling

--- shared/src/errors/error.types.ts
+++ shared/src/errors/error.types.ts
@@ -1,66 +1,16 @@
-interface ExtraInfo {
-    [infoKey: string]: string
+interface Info {
+    [infoKey: string]: string | any
 }
 
-export class BaseError extends Error {
-    extraInfo: ExtraInfo;
+export interface BaseError {
+    message: string;
     status: number;
-    constructor(extraInfo?: ExtraInfo) {
-        super();
-        this.extraInfo = extraInfo
-    }
+    info?: Info;
 }
 
-export class NotFoundError extends BaseError {
-    constructor(extraInfo?: ExtraInfo) {
-        super(extraInfo);
-        this.name = "NotFound";
-        this.message = "Page was not found";
-        this.status = ErrorStatus.NotFound;
-    }
-}
-
-export class InvalidDataError extends BaseError {
-    constructor(extraInfo?: ExtraInfo) {
-        super(extraInfo);
-        this.name = "InvalidData";
-        this.message = "Data sent were invalid";
-        this.status = ErrorStatus.InvalidData;
-    }
-}
-
-export class UnauthorizedError extends BaseError {
-    constructor(extraInfo?: ExtraInfo) {
-        super(extraInfo);
-        this.name = "Unauthorized";
-        this.message = "Please signin to your account or creat one";
-        this.status = ErrorStatus.Unauthorized;
-    }
-}
-
-export class ForbiddenError extends BaseError {
-    constructor(extraInfo?: ExtraInfo) {
-        super(extraInfo);
-        this.name = "Forbidden";
-        this.message = "You don't have access";
-        this.status = ErrorStatus.Forbidden;
-    }
-}
-
-export class NoInternetConnectionError extends BaseError {
-    constructor() {
-        super();
-        this.status = ErrorStatus.NoInternetConnection;
-        this.name = "NoInternetConnection";
-        this.message = "No internet connection";
-    }
-}
-
-export enum ErrorStatus {
-    NoInternetConnection= 420,
-    BadRequest= 400,
-    Unauthorized= 401,
-    Forbidden= 403,
-    NotFound= 404,
-    InvalidData= 422,
+export const ERRORS: {[errorName: string]: BaseError} = {
+    BadRequest: {message: "Bad request please try again", status: 400},
+    Unauthorized: {message: "Unauthenticated please signin", status: 401},
+    Forbidden: {message: "Unauthorized to use this service", status: 403},
+    NotFound: {message: "Service was not found", status: 404}
 }
